4e01859b48941af56e27734008e9e1955bc4925a
mihasar <misar2000@gmail.com>
2023-07-30 19:07:46 +0300

    First Lesson

---
+++ src/Bg.js
@@ -0,0 +1,14 @@
+import './Bg.css';
+
+function Bg() {
+    return (
+        <div className="Bg">
+            <div className='Header'>
+                <span className='Header_Text'>העלאת תמונה כדי להסיר את הרקע</span>
+                <button className='Header_btn'>העלאת תמונה</button>
+            </div>
+        </div>
+    );
+}
+
+export default Bg;
