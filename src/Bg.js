3038a96a9e16f29427d6f132bdf865ce866131b2
mihasar <misar2000@gmail.com>
2023-08-05 21:03:27 +0300

    Lesson 5

--- src/Bg.js
+++ src/Bg.js
@@ -1,17 +1,21 @@
 import './Bg.css';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import Original from './Original';
 import No_bg from './No_bg';
-// import logo from '';
-// import banner from '';
+import Eula from './Eula';
+import logo from './Assets/Images/logo.png';
+import banner from './Assets/Images/banner.png';
 
 function Bg() {
 
-    const { display_no_bg_tab, setDisplay_no_bg_tab } = useState("no");
+    const InputElement = useRef();
+
+    const [display_no_bg_tab, setDisplay_no_bg_tab] = useState("no");
+    const [showEula, setShowEula] = useState(false);
 
     function change_tab(e) {
 
-        if (e.target.classList.value == 'no_bg') {
+        if (e.target.classList.value == 'No_bg') {
             setDisplay_no_bg_tab('no');
         } else {
             setDisplay_no_bg_tab('yes');
@@ -19,12 +23,21 @@ function Bg() {
 
     }
 
+    function upload_file() {
+        InputElement.current.click();
+    }
+
+    function open_eula() {
+        setShowEula(true);
+    }
+
     return (
         <div className="Bg">
 
             <div className='Header'>
                 <span className='Header_Text'>העלאת תמונה כדי להסיר את הרקע</span>
-                <button className='Header_btn'>העלאת תמונה</button>
+                <button className='Header_btn' onClick={upload_file}>העלאת תמונה</button>
+                <input type='file' ref={InputElement} className='Input_file' />
                 <span className='Header_subtext'> פורמטים נתמכים png,jpeg</span>
             </div>
 
@@ -34,28 +47,26 @@ function Bg() {
 
                 <div className='Left_Div'>
 
-                    <div className='Main_div_tabs_Header'>
-                        <span onClick={change_tab} className='No_bg' style={{ borderBottom: display_no_bg_tab == "yes" ? "" : "3px solid #9c27B0" }}>הסר רקע</span>
-                        <span onClick={change_tab} className='Original' style={{ borderBottom: display_no_bg_tab == "yes" ? "3px solid #9c27B0" : "" }}>מקורי</span>
+                    <div className='Main_div_tabs_header' >
+                        <span onClick={change_tab} className='No_bg' style={{ borderBottom: display_no_bg_tab == "yes" ? "" : "3px solid #9C27B0" }}> הסר רקע</span>
+                        <span onClick={change_tab} className='Original' style={{ borderBottom: display_no_bg_tab == "yes" ? "3px solid #9C27B0" : "" }}> מקורי </span>
                     </div>
 
                     {display_no_bg_tab === 'yes' ?
                         <Original />
                         :
-                        <No_bg/>
+                        <No_bg />
                     }
 
-
-
                     <div className='Left_footer'>
-                        <button className='Eula_btn'>תקנון החברה</button>
+                        <button className='Eula_btn' onClick={open_eula} >תקנון החברה</button>
                         <span className='Eula_text'>ע"י העלאת תמונה אתה מסכים לתנאים ולהגבלות שלנו.</span>
+                        {showEula ? <Eula /> : ""}
                     </div>
 
                 </div>
 
                 <div className='Right_Div'>
-                    Right_Div
 
                     <div className='Right_div_middle_div'>
 
@@ -78,8 +89,8 @@ function Bg() {
                 </div>
 
                 <div className='Footer'>
-                    {/* <img src={logo} className='Logo_img' />
-                    <img src={banner} className='Banner_img' /> */}
+                    <img src={logo} className='Logo_img' />
+                    <img src={banner} className='Banner_img' />
                 </div>
 
             </div>
--- src/No_bg.js
+++ src/No_bg.js
@@ -1,15 +1,37 @@
 import './No_bg.css';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 
 function No_bg() {
 
+    const InputElement = useRef();
+
+    const [color, setColor] = useState("#000");
+
+    function choose_color() {
+        InputElement.current.click();
+    };
+
+    function change_color(e) {
+        setColor(e.target.value);
+    }
+
     return (
 
         <div className='No_bg_tab'>
-            
+
             <span className='No_bg_tab_text'>אל תשכח להוריד את הקבצים! הם ימחקו אוטומטית כאשר תצא מהדף.</span>
-        
+
+            <div className='Color_div' onClick={choose_color}>
+
+                <span className='Display_text'> צבע רקע </span>
+                <span className='Display_color' style={{backgroundColor: color}}></span>
+
+            </div>
+
+            <input type='color' ref={InputElement} className='Input_color' onChange={change_color} />
+
+
         </div>
     )
 }
